afc092b112a64e5f68f114703139f051d3105303
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2015-03-12 23:53:24 +0800

    adding put, get and purge
    also including dependencies

--- index.js
+++ index.js
@@ -2,15 +2,67 @@
 var assert = require('assert');
 
 var _ = require('lodash'),
+	debug = require('debug'),
+	makeRedisClient = require('make-redis-client'),
 	XXHash = require('xxhash');
 
 var Evaluator = require('./lib/evaluator');
 
-var includeRequestPropertiesInHash = ['uri', 'qs', 'method', 'authenticatedUser'],
-	excludeRequestHeadersFromHash = ['cookie'];
+// variables and functions
+var moduleName = 'oniyi-cache';
 
-var validHostConfigProperties = ['storePrivate', 'storeNoStore', 'ignoreNoLastMod', 'requestValidators', 'responseValidators'],
-	mergeableEvaluatorConfigProperties = ['storePrivate', 'storeNoStore', 'ignoreNoLastMod'];
+
+var logError = debug(moduleName + ':error');
+// set this namespace to log via console.error
+logError.log = console.error.bind(console); // don't forget to bind to console!
+
+var logWarn = debug(moduleName + ':warn');
+// set all output to go via console.warn
+logWarn.log = console.warn.bind(console);
+
+var logDebug = debug(moduleName + ':debug');
+// set all output to go via console.warn
+logDebug.log = console.warn.bind(console);
+
+var includeRequestPropertiesInHash = [
+	'uri',
+	'qs',
+	'method',
+	'authenticatedUser'
+];
+
+var excludeRequestHeadersFromHash = ['cookie'];
+
+var validHostConfigProperties = [
+	'storePrivate',
+	'storeNoStore',
+	'ignoreNoLastMod',
+	'requestValidators',
+	'responseValidators'
+];
+
+var mergeableEvaluatorConfigProperties = [
+	'storePrivate',
+	'storeNoStore',
+	'ignoreNoLastMod'
+];
+
+var serializableResponseProperties = [
+  // 'headers',
+  'trailers',
+  'method',
+  'statusCode',
+  'httpVersion',
+  'httpVersionMajor',
+  'httpVersionMinor'
+];
+
+function serializeResponseObject(responseObject) {
+	return JSON.stringify(_.merge(_.pick(responseObject, serializableResponseProperties), {
+		headers: _.omit(responseObject.headers, ['set-cookie']),
+		fromCache: true
+	}));
+}
 
 function OniyiCache(args) {
 	var self = this;
@@ -38,6 +90,12 @@ function OniyiCache(args) {
 
 	self.includeRequestPropertiesInHash = _.union(includeRequestPropertiesInHash, (_.isArray(args.includeRequestPropertiesInHash) ? args.includeRequestPropertiesInHash : []));
 	self.excludeRequestHeadersFromHash = _.union(excludeRequestHeadersFromHash, (_.isArray(args.excludeRequestHeadersFromHash) ? args.excludeRequestHeadersFromHash : []));
+
+	if (!args.redisClient) {
+		args.redisClient = makeRedisClient(args.redis);
+	}
+
+	self.redisClient = args.redisClient;
 }
 
 OniyiCache.prototype.hash = function(requestObject) {
@@ -144,6 +202,70 @@ OniyiCache.prototype.getEvaluator = function(hostname, config) {
 	}
 
 	return new Evaluator(cacheSettings);
+};
+
+OniyiCache.prototype.get = function(hash, callback) {
+	var self = this;
+
+	if (!_.isFunction(callback)) {
+		callback = _.noop;
+	}
+
+	self.redisClient.hgetall(hash, function(err, data) {
+		if (err) {
+			return callback(err, null);
+		}
+		if (!data.response) {
+			self.redisClient.del(hash, function(err, result) {
+				if (err) {
+					return logWarn(err);
+				}
+				logDebug('removed %d entries from cache {%s} due to invalid data', result, hash);
+			});
+			return callback(new TypeError('found invalid data in cache'));
+		}
+		data.response = JSON.parse(data.response);
+		return callback(null, data);
+	});
+};
+
+OniyiCache.prototype.put = function(data, callback) {
+	var self = this;
+
+	if (!_.isFunction(callback)) {
+		callback = _.noop;
+	}
+	// hash, response, raw, parsed, expireAt
+
+	if (_.isPlainObject(data.response)) {
+		data.response = serializeResponseObject(data.response);
+	}
+
+	var saveIt = self.redisClient.multi();
+
+	saveIt.hmset(data.hash, _.pick(data, ['response', 'raw', 'parsed']));
+
+	if (_.isNumber(data.expireAt)) {
+		saveIt.expireat(data.hash, data.expiresAt);
+	}
+
+	saveIt.exec(function(err, result) {
+		logDebug('stored data in cache. result is %s', result);
+		callback(err);
+	});
+
+};
+
+OniyiCache.prototype.purge = function(hash, callback) {
+	var self = this;
+
+	self.redisClient.del(hash, function(err, result) {
+		if (err) {
+			return logWarn(err);
+		}
+		logDebug('removed %d entries from cache {%s} due to invalid data', result, hash);
+		callback(err, result === 1);
+	});
 };
 
 module.exports = OniyiCache;
